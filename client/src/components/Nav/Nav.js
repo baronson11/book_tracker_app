90aadf1de41df43a24fa15d48a2c83be6fa5bc82
smibbie <35998407+smibbie@users.noreply.github.com>
2018-10-08 11:16:59 -0400

    first commit

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "tomato"}}>
+    <a className="navbar-brand" href="/">
+      Bookworm
+    </a>
+  </nav>
+);
+
+export default Nav;
